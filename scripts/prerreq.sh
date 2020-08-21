579967872602730d08fd7086aa838572acf0c5be
ricardor <ricardo.ruano@outlook.com>
2020-08-20 22:58:41 -0400

    Fix version de fabric sugerido por Javier Leyton, muchas gracias :)

--- scripts/prerreq.sh
+++ scripts/prerreq.sh
@@ -161,7 +161,7 @@ mkdir -p $FABRICSamplesDir
 
 sudo chmod -R 777 $FABRICSamplesDir
 cd $FABRICSamplesDir
-sudo curl -sSL http://bit.ly/2ysbOFE | bash -s 2.2
+sudo curl -sSL http://bit.ly/2ysbOFE | bash -s 2.2.0
 
 echo 'export PATH=$PATH:$HOME/hyperledger/fabric/fabric-samples/bin' >> ~/.profile
 source ~/.profile
