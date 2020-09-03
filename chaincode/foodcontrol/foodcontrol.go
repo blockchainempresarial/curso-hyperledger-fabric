83d6201df25bc4ea0232297a8147742a8680dd26
ricardor <ricardo.ruano@outlook.com>
2020-09-03 19:30:26 -0400

    Template chaincode foodcontrol

---
+++ chaincode/foodcontrol/foodcontrol.go
@@ -0,0 +1,32 @@
+/*
+Business Blockchain Training & Consulting SpA. All Rights Reserved.
+www.blockchainempresarial.com
+email: ricardo@blockchainempresarial.com
+*/
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+// SmartContract provides functions for control the food
+type SmartContract struct {
+	contractapi.Contract
+}
+
+func main() {
+
+	chaincode, err := contractapi.NewChaincode(new(SmartContract))
+
+	if err != nil {
+		fmt.Printf("Error create foodcontrol chaincode: %s", err.Error())
+		return
+	}
+
+	if err := chaincode.Start(); err != nil {
+		fmt.Printf("Error starting foodcontrol chaincode: %s", err.Error())
+	}
+}
