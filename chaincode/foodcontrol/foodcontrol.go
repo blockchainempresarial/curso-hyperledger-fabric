e0e55928060490b966e6ec6fd065b2062e5ddaa7
ricardor <ricardo.ruano@outlook.com>
2020-09-03 20:00:56 -0400

    Función set del chaincode + go.mod

--- chaincode/foodcontrol/foodcontrol.go
+++ chaincode/foodcontrol/foodcontrol.go
@@ -7,6 +7,7 @@ email: ricardo@blockchainempresarial.com
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
@@ -15,6 +16,30 @@ import (
 // SmartContract provides functions for control the food
 type SmartContract struct {
 	contractapi.Contract
+}
+
+//Food describes basic details of what makes up a food
+type Food struct {
+	Farmer  string `json:"farmer"`
+	Variety string `json:"variety"`
+}
+
+func (s *SmartContract) Set(ctx contractapi.TransactionContextInterface, foodId string, farmer string, variety string) error {
+
+	//Validaciones de sintaxis
+
+	food := Food{
+		Farmer:  farmer,
+		Variety: variety,
+	}
+
+	foodAsBytes, err := json.Marshal(food)
+	if err != nil {
+		fmt.Printf("Marshal error: %s", err.Error())
+		return err
+	}
+
+	return ctx.GetStub().PutState(foodId, foodAsBytes)
 }
 
 func main() {
---
+++ chaincode/go.mod
@@ -0,0 +1,5 @@
+module github.com/blockchainempresarial/curso-hyperledger-fabric/chaincode/foodcontrol
+
+go 1.13
+
+require github.com/hyperledger/fabric-contract-api-go v1.1.0
